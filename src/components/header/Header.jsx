748598ed0edc5718d63db824e5539aa05b01a7e5
Behruz01 <ibragimovbehruz822@gmail.com>
2023-03-15 11:56:33 +0500

    slider

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import locationIcon from "../../assets/lacation_icon.png";
 import uzb_icon from "../../assets/uzb_flag_icon.jpg";
 import uzum_market from "../../assets/uzum_market.png";
+import user_icon from "../../assets/user_icon.png";
+import heart_icon from "../../assets/heart_icon.svg";
+import bag_icon from "../../assets/bag_icon.svg";
+import catalog_icon from "../../assets/catalog_icon.png";
 
 const Header = () => {
   return (
@@ -15,7 +19,9 @@ const Header = () => {
           </div>
           <p>Buyurtmangizni 1 kunda bepul yetkazib beramiz!</p>
           <div className="flex  ">
-            <p className="mr-5 text-gray-500 hover:text-black">Savol-javoblar</p>
+            <p className="mr-5 text-gray-500 hover:text-black">
+              Savol-javoblar
+            </p>
             <p className="mr-4 text-gray-500 hover:text-black">Buyurtmalarim</p>
             <span className="flex">
               <img className="w-6 h-6" src={uzb_icon} alt="" />
@@ -27,43 +33,64 @@ const Header = () => {
       <div className="flex container mx-auto px-36 items-center justify-between p-5">
         <img className="w-56 " src={uzum_market} alt="uzum.uz" />
         <div className="flex ">
-          <button className="bg-violet-200 rounded-md p-2 text-violet-800">
+          <span className="bg-violet-200 flex rounded-md p-2 text-violet-800">
+            <img className="mr-2" src={catalog_icon} alt="" />
             Katalog
-          </button>
+          </span>
           <input
             className="w-96 border rounded-md ml-2 p-2"
             type="text"
             placeholder="Mahsulotlar va turkumlar izlash"
           />
         </div>
-        <div className="btns">
-          <span>
-            <button className="ml-2 hover:bg-gray-200 rounded-md p-2 text-black ">
-              Kirish
-            </button>
+        <div className="btns flex">
+          <span className="ml-2 flex items-center hover:bg-gray-200 rounded-md p-2 text-black ">
+            <img className="w-7 h-7 mr-2 " src={user_icon} alt="" />
+            Kirish
           </span>
-          <button className="ml-2 hover:bg-gray-200 rounded-md p-2 text-black ">
+          <span className="ml-2 flex items-center hover:bg-gray-200 rounded-md p-2 text-black ">
+            <img className="w-5 h-5 mr-2" src={heart_icon} alt="" />
             Saralangan
-          </button>
-          <button className="ml-2 hover:bg-gray-200 rounded-md p-2 text-black ">
+          </span>
+          <span className="ml-2 flex items-center hover:bg-gray-200 rounded-md p-2 text-black ">
+            <img className="w-6 h-6 mr-2" src={bag_icon} alt="" />
             Savat
-          </button>
+          </span>
         </div>
       </div>
-      <ul className="flex container mx-auto px-36 items-center gap-4">
-        <li className="text-gray-400 cursor-pointer hover:text-black">Elektronika</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Maishiy texnika</li>
+      <ul className="flex container mx-auto px-36 items-center gap-3">
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Elektronika
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Maishiy texnika
+        </li>
         <li className="text-gray-400 cursor-pointer hover:text-black">Kiyim</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Payabzallar</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Aksessuarlar</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Go'zallik</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Salomatlik</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Uy-ro'zg'or buyumlari</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Qurilish va ta'mirlash</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Avtotovarlar</li>
-        <li className="text-gray-400 cursor-pointer hover:text-black">Bolalar tovarlari</li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Payabzallar
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Aksessuarlar
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Go'zallik
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Salomatlik
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Uy-ro'zg'or buyumlari
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Qurilish va ta'mirlash
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Avtotovarlar
+        </li>
+        <li className="text-gray-400 cursor-pointer hover:text-black">
+          Bolalar tovarlari
+        </li>
         <li className="text-gray-400 cursor-pointer hover:text-black">Yana^</li>
-
       </ul>
     </>
   );
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import Slider from "../pages/slider";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       {children}
+      <Slider />
       <Footer />
     </>
   );
